72f84dfa878fa20b1f25882b2d7aefe37f1a8b26
ahoen117 <146878461+ahoen117@users.noreply.github.com>
2026-03-19 11:31:27 -0400

    Week 7 Picks Entered Save

--- RemoveWeekFromDB.py
+++ RemoveWeekFromDB.py
@@ -1,10 +1,12 @@
 import json
 import sqlite3
 
+week = 7
+
 conn = sqlite3.connect('dbcPicks.db')
 cur = conn.cursor()
 
-cur.execute("DELETE FROM picks WHERE week = 7")
+cur.execute("DELETE FROM picks WHERE week = ?", (week,))
 
 conn.commit()
 conn.close()
